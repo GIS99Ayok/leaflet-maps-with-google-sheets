e5e8f9fd3c3734cf30994103cba0dc35ebff4029
GIS99Ayok <systemgis99@gmail.com>
2025-06-29 23:22:13 +0700

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,5 +1,5 @@
 // paste in your published Google Sheets URL from the browser address bar
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/105_LzfA9P_eEdhLsZxU5RaxoMcYEDpMNT4U-PQohdGw/edit?gid=0#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1kvB0NZ8q1MK2TzCTof-KYqc5GHu5RUS65Lgzxgyv0To/edit?gid=0#gid=0';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
 var googleApiKey = 'AIzaSyBh9nKnVZm2RPeZa0ywCOxPAgJJfK87WhY';
